335ce8585a60ea8053666c3287275a56dc705811
Sakib <shahriaralamsakib7039@gmail.com>
2024-10-16 16:21:47 +0600

    vercel

--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
 // config/db.js
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'admin',
-    database: process.env.DB_NAME || 'FlightBooking',
-    port: process.env.DB_PORT || 5432,
-  });
-  
-  module.exports = {
-    query: (text, params) => pool.query(text, params),
-  };
+  connectionString: process.env.POSTGRES_URL,  // Choose the appropriate connection string
+  ssl: {
+    rejectUnauthorized: false,  // Adjust based on your SSL needs
+  },
+});
+
+module.exports = pool;
